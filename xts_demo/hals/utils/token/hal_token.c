1cc6a36a42e6c6b006cc34fb2c9fc7c6aede5c06
Jinmiao Yu <jinmiao.yu@telink-semi.com>
2022-12-29 11:29:56 +0800

    add notice file
    Change-Id: Ie008e91e36973c8de61693a7b5992b733d90a56e
    Signed-off-by: Jinmiao Yu <jinmiao.yu@telink-semi.com>

--- xts_demo/hals/utils/token/hal_token.c
+++ xts_demo/hals/utils/token/hal_token.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2022 Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ * Copyright (c) 2020 Huawei Device Co., Ltd.
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * Copyright (c) 2022 Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
  *
  *****************************************************************************/
 
